c21e843380b37130d8f2aae8f209eed57652babc
Henrik Sveinsson <henrik.sveinsson@me.com>
2014-06-06 14:07:08 +0200

    m

--- hartree-fock/sandbox/main.cpp
+++ hartree-fock/sandbox/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-    char infile[] = "/Users/henriksveinsson/git_repos/hartree-fock/hartree-fock/data/basis_sets/Be_gto6g.txt";
+    char infile[] = "/Users/henriksveinsson/git_repos/hartree-fock/hartree-fock/data/basis_sets/H_gto6g.txt";
     TurboMoleParser parser;// = new TurboMoleParser;
     std::cout << infile << std::endl;
     parser.setFile(infile);
@@ -16,15 +16,15 @@ int main()
     arma::vec nuc1pos(3);
     nuc1pos.zeros();
     std::vector<Contracted> contracted1 = parser.returnContracted(nuc1pos);
-    Nucleus nuc1(nuc1pos, 4);
+    Nucleus nuc1(nuc1pos, 1);
 
     arma::vec nuc2pos(3);
     nuc2pos.zeros();
-    nuc2pos(0) = 4.63;
+    nuc2pos(0) = 1.4;
     std::vector<Contracted> contracted2 = parser.returnContracted(nuc2pos);
-    Nucleus nuc2(nuc2pos, 4);
+    Nucleus nuc2(nuc2pos, 1);
 
-    ElectronSystem system(4);
+    ElectronSystem system(1);
     for (Contracted contracted : contracted1) {
         system.addContracted(contracted);
     }
@@ -35,7 +35,8 @@ int main()
     system.addNucleus(nuc2);
 
     HFSolver solver(system);
-    solver.solve();
+    double energy = solver.solve();
+    std::cout << "Energy " << energy << std::endl;
 
     return 0;
 }
